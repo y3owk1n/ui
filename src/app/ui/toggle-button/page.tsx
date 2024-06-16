3cd7dd63e3d6e87f78ed2c31985aae260ebfb61b
y3owk1n <wongyeowkin@gmail.com>
2024-06-17 02:31:14 +0800

    feat: add toggle button component

---
+++ src/app/ui/toggle-button/page.tsx
@@ -0,0 +1,18 @@
+import { ToggleButton } from "@/components/ui/toggle-button";
+import { Bold } from "lucide-react";
+
+export default function ButtonPage() {
+	return (
+		<div className="flex gap-4">
+			<ToggleButton aria-label="Toggle Bold">
+				<Bold className="h-4 w-4" />
+			</ToggleButton>
+			<ToggleButton variant="outline" aria-label="Toggle Bold">
+				<Bold className="h-4 w-4" />
+			</ToggleButton>
+			<ToggleButton isDisabled aria-label="Toggle Bold">
+				<Bold className="h-4 w-4" />
+			</ToggleButton>
+		</div>
+	);
+}
---
+++ src/components/ui/toggle-button.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import {
+	ToggleButton as _ToggleButton,
+	type ToggleButtonProps as _ToggleButtonProps,
+} from "react-aria-components";
+
+import { cn } from "@/lib/utils";
+
+const toggleButtonVariants = cva(
+	"inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors data-[disabled]:pointer-events-none data-[hovered]:bg-muted data-[selected]:bg-accent data-[hovered]:text-muted-foreground data-[selected]:text-accent-foreground data-[disabled]:opacity-50 data-[focus-visible]:outline-none data-[focus-visible]:ring-2 data-[focus-visible]:ring-ring data-[focus-visible]:ring-offset-2",
+	{
+		variants: {
+			variant: {
+				default: "bg-transparent",
+				outline:
+					"border border-input bg-transparent data-[hovered]:bg-accent data-[hovered]:text-accent-foreground",
+			},
+			size: {
+				default: "h-10 px-3",
+				sm: "h-9 px-2.5",
+				lg: "h-11 px-5",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+			size: "default",
+		},
+	},
+);
+
+export interface ToggleButtonProps
+	extends _ToggleButtonProps,
+		VariantProps<typeof toggleButtonVariants> {}
+
+const ToggleButton = React.forwardRef<
+	React.ElementRef<typeof _ToggleButton>,
+	ToggleButtonProps
+>(({ className, variant, size, ...props }, ref) => (
+	<_ToggleButton
+		ref={ref}
+		className={cn(toggleButtonVariants({ variant, size, className }))}
+		{...props}
+	/>
+));
+
+ToggleButton.displayName = _ToggleButton.name;
+
+export { ToggleButton, toggleButtonVariants };
