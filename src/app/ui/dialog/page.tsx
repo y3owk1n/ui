6510184c5699d8212d2619d39468d692078350dd
y3owk1n <wongyeowkin@gmail.com>
2024-06-17 15:06:34 +0800

    feat: add dialog

---
+++ src/app/ui/dialog/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogOverlay,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Copy } from "lucide-react";
+
+export default function CheckboxPage() {
+	return (
+		<div className="flex gap-4">
+			<DialogTrigger>
+				<Button variant="outline">Normal Dialog</Button>
+				<DialogOverlay>
+					<DialogContent className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>Edit profile</DialogTitle>
+							<p className="text-sm text-muted-foreground">
+								Make changes to your profile here. Click save
+								when you&apos;re done.
+							</p>
+						</DialogHeader>
+						<div className="grid gap-4 py-4">
+							<div className="grid grid-cols-4 items-center gap-4">
+								<Label htmlFor="name" className="text-right">
+									Name
+								</Label>
+								<Input
+									id="name"
+									defaultValue="Pedro Duarte"
+									className="col-span-3"
+								/>
+							</div>
+							<div className="grid grid-cols-4 items-center gap-4">
+								<Label
+									htmlFor="username"
+									className="text-right"
+								>
+									Username
+								</Label>
+								<Input
+									id="username"
+									defaultValue="@peduarte"
+									className="col-span-3"
+								/>
+							</div>
+						</div>
+						<DialogFooter>
+							<Button type="submit">Save changes</Button>
+						</DialogFooter>
+					</DialogContent>
+				</DialogOverlay>
+			</DialogTrigger>
+			<DialogTrigger>
+				<Button variant="outline">Alert Dialog</Button>
+				<DialogOverlay isDismissable={false}>
+					<DialogContent role="alertdialog" closeButton={false}>
+						{({ close }) => (
+							<>
+								<DialogHeader>
+									<DialogTitle>
+										Are you absolutely sure?
+									</DialogTitle>
+								</DialogHeader>
+								<p className="text-sm text-muted-foreground">
+									This action cannot be undone. This will
+									permanently delete your account and remove
+									your data from our servers.
+								</p>
+								<DialogFooter>
+									<Button
+										variant="outline"
+										autoFocus
+										onPress={close}
+										className="mt-2 sm:mt-0"
+									>
+										Cancel
+									</Button>
+									<Button
+										variant="destructive"
+										onPress={close}
+									>
+										Continue
+									</Button>
+								</DialogFooter>
+							</>
+						)}
+					</DialogContent>
+				</DialogOverlay>
+			</DialogTrigger>
+			<DialogTrigger>
+				<Button variant="outline">Sheet</Button>
+				<DialogOverlay>
+					<DialogContent side="right" className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>Edit profile</DialogTitle>
+							<p className="text-sm text-muted-foreground">
+								Make changes to your profile here. Click save
+								when you&apos;re done.
+							</p>
+						</DialogHeader>
+						<div className="grid gap-4 py-4">
+							<div className="grid grid-cols-4 items-center gap-4">
+								<Label htmlFor="name" className="text-right">
+									Name
+								</Label>
+								<Input
+									id="name"
+									defaultValue="Pedro Duarte"
+									className="col-span-3"
+								/>
+							</div>
+							<div className="grid grid-cols-4 items-center gap-4">
+								<Label
+									htmlFor="username"
+									className="text-right"
+								>
+									Username
+								</Label>
+								<Input
+									id="username"
+									defaultValue="@peduarte"
+									className="col-span-3"
+								/>
+							</div>
+						</div>
+						<DialogFooter>
+							<Button type="submit">Save changes</Button>
+						</DialogFooter>
+					</DialogContent>
+				</DialogOverlay>
+			</DialogTrigger>
+		</div>
+	);
+}
