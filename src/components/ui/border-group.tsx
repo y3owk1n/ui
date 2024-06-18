2b1cf6e8c62bb64740a5fe07275f47acf9975238
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-06-18 14:24:14 +0800

    feat: format border-group

--- src/components/ui/border-group.tsx
+++ src/components/ui/border-group.tsx
@@ -13,7 +13,7 @@ const BorderGroup = React.forwardRef<HTMLDivElement, BorderGroupProps>(
 				className={cn(
 					"flex w-full flex-row flex-nowrap",
 					orientation === "horizontal"
-						? " [&>*:first-child]:rounded-none [&>*:first-child]:rounded-l-md [&>*:first-child]:border-r-0 [&>*:last-child]:rounded-none [&>*:last-child]:rounded-r-md [&>*:last-child]:border-l-0 [&>*:not(:first-child):not(:last-child):not(:nth-child(2))]:border-l-0 [&>*:not(:first-child):not(:last-child)]:rounded-none [&>*:only-child]:rounded-md"
+						? "[&>*:first-child]:rounded-none [&>*:first-child]:rounded-l-md [&>*:first-child]:border-r-0 [&>*:last-child]:rounded-none [&>*:last-child]:rounded-r-md [&>*:last-child]:border-l-0 [&>*:not(:first-child):not(:last-child):not(:nth-child(2))]:border-l-0 [&>*:not(:first-child):not(:last-child)]:rounded-none [&>*:only-child]:rounded-md"
 						: "",
 					orientation === "vertical"
 						? "[&>*:first-child]:rounded-none [&>*:first-child]:rounded-t-md [&>*:first-child]:border-b-0 [&>*:last-child]:rounded-none [&>*:last-child]:rounded-b-md [&>*:last-child]:border-t-0 [&>*:not(:first-child):not(:last-child):not(:nth-child(2))]:rounded-none [&>*:not(:first-child):not(:last-child):not(:nth-child(2))]:border-t-0 [&>*:nth-child(2)]:border-t [&>*:only-child]:rounded-md"
