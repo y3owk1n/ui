649a3a3ffbd8adf62add84357e01100f2bf43fdf
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-06-19 11:48:38 +0800

    feat: update file-trigger

---
+++ src/app/ui/file-trigger/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { FileItems, FileTrigger } from "@/components/ui/file-trigger";
+
+export default function ButtonPage() {
+	return (
+		<div className="grid gap-4">
+			<FileTrigger acceptedFileTypes={["image/*"]}>
+				<div className="grid gap-2">
+					<div>
+						<Button>Upload image</Button>
+					</div>
+					<FileItems>
+						{(files) => (
+							<Card>
+								<CardHeader>
+									<CardTitle>File</CardTitle>
+								</CardHeader>
+								<CardContent>
+									{files.map((file) => (
+										<CardDescription key={file}>
+											{file}
+										</CardDescription>
+									))}
+								</CardContent>
+							</Card>
+						)}
+					</FileItems>
+				</div>
+			</FileTrigger>
+			<FileTrigger>
+				<div className="grid gap-2">
+					<div>
+						<Button>Upload file</Button>
+					</div>
+					<FileItems>
+						{(files) => (
+							<Card>
+								<CardHeader>
+									<CardTitle>File</CardTitle>
+								</CardHeader>
+								<CardContent>
+									{files.map((file) => (
+										<CardDescription key={file}>
+											{file}
+										</CardDescription>
+									))}
+								</CardContent>
+							</Card>
+						)}
+					</FileItems>
+				</div>
+			</FileTrigger>
+			<FileTrigger allowsMultiple>
+				<div className="grid gap-2">
+					<div>
+						<Button>Upload file(s)</Button>
+					</div>
+					<FileItems>
+						{(files) => (
+							<Card>
+								<CardHeader>
+									<CardTitle>Files</CardTitle>
+								</CardHeader>
+								<CardContent>
+									{files.map((file) => (
+										<CardDescription key={file}>
+											{file}
+										</CardDescription>
+									))}
+								</CardContent>
+							</Card>
+						)}
+					</FileItems>
+				</div>
+			</FileTrigger>
+			<FileTrigger acceptDirectory>
+				<div className="grid gap-2">
+					<div>
+						<Button>Upload directory</Button>
+					</div>
+					<FileItems>
+						{(files) => (
+							<Card>
+								<CardHeader>
+									<CardTitle>Files</CardTitle>
+								</CardHeader>
+								<CardContent>
+									<div className="grid gap-1">
+										{files.map((file) => (
+											<CardDescription key={file}>
+												{file}
+											</CardDescription>
+										))}
+									</div>
+								</CardContent>
+							</Card>
+						)}
+					</FileItems>
+				</div>
+			</FileTrigger>
+		</div>
+	);
+}
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 "use client";
+import { type VariantProps, cva } from "class-variance-authority";
 import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 import { baseVariant, unstyledVariant } from "@/lib/variants";
