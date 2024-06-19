c00454619c2dc8a98262c0068040fd3478ebc5e8
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-06-19 10:53:11 +0800

    feat: add badge component

---
+++ src/app/ui/badge/page.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+
+export default function CheckboxPage() {
+	return (
+		<div className="flex flex-wrap gap-4">
+			<Badge>Badge</Badge>
+			<Badge variant="outline">Badge</Badge>
+			<Badge variant="destructive">Badge</Badge>
+			<Badge variant="secondary">Badge</Badge>
+			<Badge variant="unstyled">Badge</Badge>
+		</div>
+	);
+}
