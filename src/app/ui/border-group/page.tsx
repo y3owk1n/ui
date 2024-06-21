e61f10ae48aab3bae661b37d05e70ac57a67017f
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-06-21 19:04:35 +0800

    feat: add a bordergroup usecase

--- src/app/ui/border-group/page.tsx
+++ src/app/ui/border-group/page.tsx
@@ -1,9 +1,36 @@
+"use client";
 import { BorderGroup } from "@/components/ui/border-group";
 import { Button } from "@/components/ui/button";
+import { Menu, MenuItem, MenuPopover, MenuTrigger } from "@/components/ui/menu";
+import { ChevronDown } from "lucide-react";
 
 export default function ButtonPage() {
 	return (
 		<div className="flex flex-col gap-4">
+			<BorderGroup>
+				<Button variant="outline">Create a merge commit</Button>
+
+				<MenuTrigger>
+					<Button aria-label="Menu" variant="outline" size="icon">
+						<ChevronDown className="h-4 w-4" />
+					</Button>
+					<MenuPopover className="min-w-[8rem]">
+						<Menu
+							onAction={(key) =>
+								typeof window !== "undefined"
+									? alert(key)
+									: console.log(key)
+							}
+						>
+							<MenuItem id="create">
+								Create a merge commit
+							</MenuItem>
+							<MenuItem id="squash">Squash and merge</MenuItem>
+							<MenuItem id="rebase">Rebase and merge</MenuItem>
+						</Menu>
+					</MenuPopover>
+				</MenuTrigger>
+			</BorderGroup>
 			<BorderGroup>
 				<Button variant="destructive">hello</Button>
 				<Button>hello</Button>
