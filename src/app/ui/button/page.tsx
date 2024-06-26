8792b9f9244f355949ba8681d384638a6ee4a97b
y3owk1n <wongyeowkin@gmail.com>
2024-06-26 23:21:20 +0800

    feat: add more variont to theme

--- src/app/ui/button/page.tsx
+++ src/app/ui/button/page.tsx
@@ -5,11 +5,13 @@ export default function ButtonPage() {
 		<div className="flex gap-4">
 			<Button>Default</Button>
 			<Button variant="destructive">Destructive</Button>
+			<Button variant="success">Success</Button>
+			<Button variant="warning">Warning</Button>
+			<Button variant="info">Info</Button>
 			<Button variant="outline">Outline</Button>
 			<Button variant="secondary">Secondary</Button>
 			<Button variant="ghost">Ghost</Button>
 			<Button variant="link">Link</Button>
-			<Button>Destructive</Button>
 			<Button isDisabled>disabled</Button>
 			<Button isLoading>loading</Button>
 		</div>
