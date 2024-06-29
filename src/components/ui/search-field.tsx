ac9a7a0841385b536bf8ed90b14bceae60164bad
y3owk1n <wongyeowkin@gmail.com>
2024-06-29 23:34:19 +0800

    feat: do not allow type props to search field

--- src/components/ui/search-field.tsx
+++ src/components/ui/search-field.tsx
@@ -33,35 +33,37 @@ const SearchField = React.forwardRef<HTMLDivElement, SearchFieldProps>(
 );
 SearchField.displayName = "SearchField";
 
-const SearchFieldInput = React.forwardRef<HTMLInputElement, InputProps>(
-	({ className, ...props }, ref) => {
-		return (
-			<Group className="relative">
-				<div className="absolute left-0 top-0 grid h-full w-10 place-items-center">
-					<Search className="size-4 stroke-input" />
-				</div>
-				<Input
-					className={(values) =>
-						cn(
-							"px-10 [&::-webkit-search-cancel-button]:hidden",
-							typeof className === "function"
-								? className(values)
-								: className,
-						)
-					}
-					ref={ref}
-					{...props}
-				/>
+const SearchFieldInput = React.forwardRef<
+	HTMLInputElement,
+	Omit<InputProps, "type">
+>(({ className, ...props }, ref) => {
+	return (
+		<Group className="relative">
+			<div className="absolute left-0 top-0 grid h-full w-10 place-items-center">
+				<Search className="size-4 stroke-input" />
+			</div>
+			<Input
+				type="search"
+				className={(values) =>
+					cn(
+						"px-10 [&::-webkit-search-cancel-button]:hidden",
+						typeof className === "function"
+							? className(values)
+							: className,
+					)
+				}
+				ref={ref}
+				{...props}
+			/>
 
-				<div className="absolute right-0 top-0 h-10">
-					<Button variant="unstyled" size="icon">
-						<X className="size-4" />
-					</Button>
-				</div>
-			</Group>
-		);
-	},
-);
+			<div className="absolute right-0 top-0 h-10">
+				<Button variant="unstyled" size="icon">
+					<X className="size-4" />
+				</Button>
+			</div>
+		</Group>
+	);
+});
 SearchFieldInput.displayName = "SearchFieldInput";
 
 export { SearchField, type SearchFieldProps, SearchFieldInput };
