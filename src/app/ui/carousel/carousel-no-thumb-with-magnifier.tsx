0bd679b72e32ebe0d763e67c122ea80068bd2381
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-09 14:36:19 +0800

    feat: add carousel with magnifier

---
+++ src/app/ui/carousel/carousel-no-thumb-with-magnifier.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+import {
+	Carousel,
+	type CarouselApi,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+	ImageMagnifier,
+	ImageMagnifierSource,
+	ImageMagnifierTarget,
+} from "@/components/ui/image-magnifier";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+interface CarouselLeftThumbProps {
+	images: string[];
+}
+
+export default function CarouselNoThumbWithMagnifier({
+	images,
+}: CarouselLeftThumbProps) {
+	const mainRef = useRef<HTMLDivElement>(null);
+
+	const [mainApi, setMainApi] = useState<CarouselApi>();
+
+	const [selectedIndex, setSelectedIndex] = useState(0);
+
+	const [count, setCount] = useState(0);
+
+	const onSelect = useCallback(() => {
+		if (!mainApi) return;
+		setSelectedIndex(mainApi.selectedScrollSnap());
+	}, [mainApi]);
+
+	useEffect(() => {
+		if (!mainApi) return;
+		onSelect();
+
+		setCount(mainApi.scrollSnapList().length);
+
+		mainApi.on("select", onSelect).on("reInit", onSelect);
+	}, [mainApi, onSelect]);
+
+	return (
+		<div className="flex w-full max-w-[500px] flex-col gap-2">
+			<Carousel className="w-full" setApi={setMainApi}>
+				<CarouselContent className="gap-1" ref={mainRef}>
+					{images.map((image, index) => (
+						<CarouselItem key={index} className="">
+							<ImageMagnifier imageUrl={image}>
+								<ImageMagnifierSource className="aspect-square h-full w-full rounded-md border bg-gray-100 bg-muted object-contain transition-all duration-200" />
+								<ImageMagnifierTarget />
+							</ImageMagnifier>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+
+				<CarouselPrevious className="left-4" />
+				<CarouselNext className="right-4" />
+				<Badge variant="outline" className="absolute right-0 top-0 m-2">
+					{selectedIndex + 1} of {count}
+				</Badge>
+			</Carousel>
+		</div>
+	);
+}
--- src/app/ui/carousel/page.tsx
+++ src/app/ui/carousel/page.tsx
@@ -2,6 +2,7 @@ import Preview from "@/components/preview";
 import CarouselBottomThumb from "./carousel-bottom-thumb";
 import CarouselLeftThumb from "./carousel-left-thumb";
 import CarouselNoThumb from "./carousel-no-thumb";
+import CarouselNoThumbWithMagnifier from "./carousel-no-thumb-with-magnifier";
 
 const images = [
 	"https://images.unsplash.com/photo-1720072480814-e1e5bf1937ca?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
@@ -34,6 +35,9 @@ export default function CarouselPage() {
 			</Preview>
 			<Preview>
 				<CarouselNoThumb images={images} />
+			</Preview>
+			<Preview>
+				<CarouselNoThumbWithMagnifier images={images} />
 			</Preview>
 		</div>
 	);
