5eb263218f6be56c1254187cacf78b8dbae3a249
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-11 14:14:41 +0800

    feat: add callout to accordion

--- src/components/callout.tsx
+++ src/components/callout.tsx
@@ -1,14 +1,27 @@
-import { Alert, AlertDescription, AlertTitle } from "@/registry/ui/alert";
+import {
+	Alert,
+	AlertDescription,
+	AlertTitle,
+	type alertVariants,
+} from "@/registry/ui/alert";
+import { type VariantProps } from "class-variance-authority";
 
 interface CalloutProps {
 	icon?: string;
 	title?: string;
 	children?: React.ReactNode;
+	variant?: VariantProps<typeof alertVariants>["variant"];
 }
 
-export function Callout({ title, children, icon, ...props }: CalloutProps) {
+export function Callout({
+	title,
+	children,
+	icon,
+	variant = "warning",
+	...props
+}: CalloutProps) {
 	return (
-		<Alert {...props}>
+		<Alert variant={variant} {...props}>
 			{icon && <span className="mr-4 text-2xl">{icon}</span>}
 			{title && <AlertTitle>{title}</AlertTitle>}
 			<AlertDescription>{children}</AlertDescription>
