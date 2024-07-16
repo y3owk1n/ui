afd919acc23f463814da66e4f006c633207923c3
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-16 12:37:51 +0800

    fix: double keyframes entry in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,14 +88,6 @@ const config = {
 					"0%,70%,100%": { opacity: "1" },
 					"20%,50%": { opacity: "0" },
 				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-				"caret-blink": "caret-blink 1.25s ease-out infinite",
-				ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
-			},
-			keyframes: {
 				ripple: {
 					"0%, 100%": {
 						transform: "translate(-50%, -50%) scale(1)",
@@ -104,6 +96,12 @@ const config = {
 						transform: "translate(-50%, -50%) scale(0.9)",
 					},
 				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				"caret-blink": "caret-blink 1.25s ease-out infinite",
+				ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
 			},
 		},
 	},
