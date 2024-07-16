f0c28cac5f54b3afbfefe4187d596229f1a6363b
mtraworld <140996996+mtraworld@users.noreply.github.com>
2024-07-16 12:24:44 +0800

    feat: prevent zoom in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { QueryClientProvider } from "@/components/query-client-provider";
@@ -13,6 +13,15 @@ const fontSans = FontSans({
 	subsets: ["latin"],
 	variable: "--font-sans",
 });
+
+export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
+	// Also supported by less commonly used
+	// interactiveWidget: 'resizes-visual',
+};
 
 export const metadata: Metadata = {
 	title: "Create Next App",
--- src/registry/ui/accordion.tsx
+++ src/registry/ui/accordion.tsx
@@ -48,15 +48,17 @@ AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
 const AccordionContent = React.forwardRef<
 	React.ElementRef<typeof AccordionPrimitive.Content>,
 	React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-	<AccordionPrimitive.Content
-		ref={ref}
-		className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-		{...props}
-	>
-		<div className={cn("pb-4 pt-0", className)}>{children}</div>
-	</AccordionPrimitive.Content>
-));
+>(({ className, children, ...props }, ref) => {
+	return (
+		<AccordionPrimitive.Content
+			ref={ref}
+			className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+			{...props}
+		>
+			<div className={cn("pb-4 pt-0", className)}>{children}</div>
+		</AccordionPrimitive.Content>
+	);
+});
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
